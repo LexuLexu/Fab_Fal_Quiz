b4aa3b57ebc4f40d6f5104c7646036b21dcd525c
LexuLexu <pepsiandram@gmail.com>
2020-08-21 21:07:16 +0100

    added sophie quote and descriptions minor ui changes

--- app/src/main/java/com/example/whichfalmouthmemberareyou/ResultActivity.kt
+++ app/src/main/java/com/example/whichfalmouthmemberareyou/ResultActivity.kt
@@ -21,6 +21,11 @@ class ResultActivity : AppCompatActivity() {
         resultText.setText(mostLike.capitalize())
 
         val resultImage = findViewById<ImageView>(R.id.resultImage)
+        val sophieQuote = findViewById<TextView>(R.id.sophieQuote)
+        val description = findViewById<TextView>(R.id.descriptions)
+
+        val quotesArray: Array<String> = resources.getStringArray(R.array.sophie_quotes)
+        val descArray: Array<String> = resources.getStringArray(R.array.descriptions)
 
         when (mostLike) {
 
@@ -29,6 +34,26 @@ class ResultActivity : AppCompatActivity() {
             "jack" -> resultImage.setImageDrawable(getDrawable(R.drawable.jack_result))
             "sam" -> resultImage.setImageDrawable(getDrawable(R.drawable.sam_result))
             "tom" -> resultImage.setImageDrawable(getDrawable(R.drawable.tom_result))
+
+        }
+
+        when (mostLike) {
+
+            "alex" -> sophieQuote.setText("Sophie says: \"" + quotesArray[0] + "\"")
+            "amy" -> sophieQuote.setText("Sophie says: \"" + quotesArray[1] + "\"")
+            "jack" -> sophieQuote.setText("Sophie says: \"" + quotesArray[2] + "\"")
+            "sam" -> sophieQuote.setText("Sophie says: \"" + quotesArray[3] + "\"")
+            "tom" -> sophieQuote.setText("Sophie says: \"" + quotesArray[4] + "\"")
+
+        }
+
+        when (mostLike) {
+
+            "alex" -> description.setText(descArray[0])
+            "amy" -> description.setText(descArray[1])
+            "jack" -> description.setText(descArray[2])
+            "sam" -> description.setText(descArray[3])
+            "tom" -> description.setText(descArray[4])
 
         }
 
